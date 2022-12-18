d4b0f9ebc9db8d09d5f31bc13132e7f12cff5fd1
Anton Gadiyatov <gadiyatov.an@gmail.com>
2022-12-18 17:47:25 +0300

    feat: added readline interface, greeting and closing messages

--- file-manager/app/app.js
+++ file-manager/app/app.js
@@ -4,6 +4,7 @@
  */
 
 // Dependencies
+import readline from "node:readline";
 import { cli } from "../modules/index.js";
 
 // Instantiate the app object
@@ -16,4 +17,32 @@ app.init = () => {
   // Colorize username
   const colorizedUsername = cli.colorizer.colorizeString(33, username);
   cli.cliDrawer.centered(`Welcome to the File Manager, ${colorizedUsername}!`);
+
+  // Start the interface
+  const _interface = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: "",
+  });
+
+  // Create an initial prompt
+  _interface.prompt(123);
+
+  // Handle each line of input separately
+  _interface.on("line", (string) => {
+    // Send to the input processor
+    app.processInput(string);
+  });
+
+  // On killing the interface with CTRL + C
+  _interface.on("close", () => {
+    process.exit(0);
+  });
+
+  // On killing the process
+  process.on("exit", () => {
+    cli.cliDrawer.centered(
+      `Thank you for using File Manager, ${colorizedUsername}, goodbye!`
+    );
+  });
 };
