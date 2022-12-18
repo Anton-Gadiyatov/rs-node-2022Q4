e1e2cb0107f925f567a06237cf2bd80944c03d4f
Anton Gadiyatov <gadiyatov.an@gmail.com>
2022-12-18 18:00:42 +0300

    feat: app now starting with home working directory

--- file-manager/app/app.js
+++ file-manager/app/app.js
@@ -5,6 +5,7 @@
 
 // Dependencies
 import readline from "node:readline";
+import os from "node:os";
 import { cli } from "../modules/index.js";
 
 // Instantiate the app object
@@ -12,11 +13,25 @@ export const app = {};
 
 // Init function
 app.init = () => {
+  // Get user home directory
+  const homeDir = os.homedir();
+
+  // Move to user home directory
+  try {
+    process.chdir(homeDir);
+  } catch (err) {
+    console.error(`Cannot move into user home directory: ${err}`);
+  }
+
   // Get username from args
   const { username } = cli.args.parseInitArgs();
   // Colorize username
   const colorizedUsername = cli.colorizer.colorizeString(33, username);
   cli.cliDrawer.centered(`Welcome to the File Manager, ${colorizedUsername}!`);
+  // Loging current working directory
+  console.log(
+    cli.colorizer.colorizeString(32, `You are currently in ${process.cwd()}`)
+  );
 
   // Start the interface
   const _interface = readline.createInterface({
@@ -29,9 +44,13 @@ app.init = () => {
   _interface.prompt(123);
 
   // Handle each line of input separately
-  _interface.on("line", (string) => {
+  _interface.on("line", async (string) => {
     // Send to the input processor
-    app.processInput(string);
+    await app.processInput(string);
+    // Loging current working directory
+    console.log(
+      cli.colorizer.colorizeString(32, `You are currently in ${process.cwd()}`)
+    );
   });
 
   // On killing the interface with CTRL + C
