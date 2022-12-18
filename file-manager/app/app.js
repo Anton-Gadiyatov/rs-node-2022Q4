edc4982102115cbe94379a11d9683d3083d19da5
Anton Gadiyatov <gadiyatov.an@gmail.com>
2022-12-18 16:44:37 +0300

    init: init file-magager project

---
+++ file-manager/app/app.js
@@ -0,0 +1,10 @@
+/*
+ * Main file of file-manager
+ *
+ */
+export const app = {};
+
+// Init function
+app.init = () => {
+  console.log("Hello World");
+};
---
+++ file-manager/index.js
@@ -0,0 +1,8 @@
+/*
+ * Primary file for starting file-manager
+ *
+ */
+import { app } from "./app/index.js";
+
+// Starting the app
+app.init();
