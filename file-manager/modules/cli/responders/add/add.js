d0b271a43b237ce939b54feb071c072296b739d6
Anton Gadiyatov <gadiyatov.an@gmail.com>
2022-12-19 20:02:07 +0300

    feat: add add command support

--- file-manager/modules/cli/responders/add/add.js
+++ file-manager/modules/cli/responders/add/add.js
@@ -4,21 +4,32 @@
  */
 
 // Dependencies
-import { readdir } from "node:fs/promises";
-import { cliDrawer } from "../../utils/cliDrawer.js";
-import { isFile } from "../../utils/isFile.js";
+import { resolve } from "node:path";
+import { open } from "node:fs/promises";
 
-// Creating empty file in current working directory
-export const lsResponder = async (string, callback) => {
+// Creating empty file or folder in current working directory
+export const addResponder = async (string, callback) => {
   string =
     typeof string === "string" && string.trim().length > 0
       ? string.trim()
       : false;
   if (string) {
+    let fileHandle;
+    const array = string.split(" ");
+    const fileName =
+      typeof array[1] === "string" && array[1].trim().length > 0
+        ? array[1].trim()
+        : false;
+
     try {
+      const pathToFile = resolve(process.cwd(), fileName);
+      fileHandle = await open(pathToFile, "w");
+      callback();
     } catch (err) {
       console.log("Invalid input");
       callback();
+    } finally {
+      fileHandle?.close();
     }
   } else {
     console.log("Invalid input");
