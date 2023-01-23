24d3820697499168bba60215614141c946874591
Гадиятов Антон Николаевич <AGadiyatov@IBS.RU>
2023-01-23 20:16:49 +0300

    feat: added basic stream implementation

--- remote-control/index.js
+++ remote-control/index.js
@@ -1,6 +1,6 @@
 import { Readable } from "node:stream";
 import { httpServer } from "./src/http_server/index.js";
-import { WebSocketServer } from "ws";
+import { WebSocketServer, createWebSocketStream } from "ws";
 import { messageHandlers } from "./src/handlers/message-handlers.js";
 import { mouse, left, right, up, down } from "@nut-tree/nut-js";
 
@@ -13,27 +13,17 @@ const wss = new WebSocketServer({ port: 8080 });
 
 wss.on("connection", (ws) => {
   console.log("connected");
-  ws.on("message", (data) => {
-    let message = "";
-    const readable = Readable.from(data);
+  const stream = createWebSocketStream(ws, { encoding: "utf-8" });
+  stream.on("data", (message) => {
+    try {
+      console.log(message);
+    } catch (err) {
+      console.log(err);
+    }
+    ws.send(message);
+  });
 
-    readable.on("data", (chunk) => {
-      message += chunk;
-    });
-
-    readable.on("end", () => {
-      try {
-        const returningMessage = messageHandlers(message);
-        ws.send(returningMessage);
-      } catch (err) {
-        console.log(err);
-        ws.send(message);
-      }
-    });
-    //     console.log(message.toString());
-    //     (async () => {
-    //       await mouse.move(left(10));
-    //     })();
-    //
+  ws.on("close", () => {
+    console.log("disconnected");
   });
 });
